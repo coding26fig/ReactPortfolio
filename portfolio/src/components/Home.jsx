a23348210eb59363bd521901658c1cf757f6239b
Charnay Birton <coding26fig@gmail.com>
2022-08-06 12:03:38 -0400

    functional navbar

---
+++ portfolio/src/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ()
+const Home = () => {
+  return (
+    <div name='home' className='w-full h-screen bg-[#0a192f]'></div>
+  )
+}
+
+export default Home
--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import {FaBars,FaTimes,} from 'react-icons/fa';
 import Logo from '../assets/logo.png'
+import { useState } from 'react';
 const Navbar = () => {
+  const [nav, setNav] = useState(false)
+  const handleClick = () => setNav(!nav)
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div>
@@ -9,35 +12,46 @@ const Navbar = () => {
       </div>
 
        {/*menu*/}
-      <div> 
-          <ul className='flex'>
+      
+          <ul className='hidden md:flex'>
           <li>Home</li>
           <li>About</li>
           <li>Projects</li>
           <li>Resume</li>
           <li>Contact Me</li>
         </ul>
-        </div>
+        
 
         {/*Hamburger menu*/}
-        <div className='hidden'>
-          <FaBars/>
+       <div onClick={handleClick} className='md:hidden z-10'>
+         {!nav ? <FaBars /> : <FaTimes />}
           </div>
 
 
         {/*Mobile menu*/}
-        <ul className='hidden'>
-          <li>Home</li>
-          <li>About</li>
-          <li>Projects</li>
-          <li>Resume</li>
-          <li>Contact Me</li>
+        <ul
+        className={
+          !nav
+            ? 'hidden'
+            : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+        }
+      >
+          <li className='py-6 text-4xl'>About</li> 
+          <li className='py-6 text-4xl'>Home</li>
+          <li className='py-6 text-4xl'>Projects</li>
+          <li className='py-6 text-4xl'>Resume</li>
+          <li className='py-6 text-4xl'>Contact Me</li>
         </ul>
 
 
-        
+
         {/*Social icons*/}
-        <div className='hidden'></div>
+        <div className='flex fixed flex-col top [35%] left-0'></div>
+        <ul>
+          <li> 
+            <a href='www.linkedin.com/in/charnay-birton'>LinkedIn</a>
+          </li>
+        </ul>
     </div>
   )
 }
