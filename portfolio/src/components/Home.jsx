6094c0f8983f1e4736a08fefbb7231b37ac05911
Charnay Birton <coding26fig@gmail.com>
2022-08-06 15:17:46 -0400

    adding about page

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -11,12 +11,16 @@ const Home = () => {
     <p className='text-pink-600'>Hi my name is</p>
     <h1 className='text-4xl sm:7xl font-bold text-[#ccd6f6]'>Charnay Birton </h1>
     
-    <p>I am a full-stack developer specializing in designing, developing, and maintaining scalable software. 
+    <p className='text-[#8892b0] py-4 max-w-[700px]'>I am a full-stack developer specializing in designing, developing, and maintaining scalable software. 
         With a background in Project coordinating and user expirience. I am currently focused on curating web 
         applications that encompass both user and business-centric elements that encourage user retention and generates revenue and value.</p>
 
 <div>
-    <button> View Work <HiArrowNarrowRight/></button>
+    <button className='text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600'> View Work 
+    <span>
+<HiArrowNarrowRight className='ml-3 '/>
+    </span>
+    </button>
 </div>
 
 </div>
