d3ad96bee56a30ac13f43f21dd6a1f833106cfcc
Charnay Birton <coding26fig@gmail.com>
2022-08-07 07:15:17 -0400

    projects

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -3,6 +3,9 @@ import Project1 from '../assets/gitcocktail.png';
 import Project2 from '../assets/Passwordgenerator.png';
 import Project3 from '../assets/Stockalert.png';
 import Project4 from '../assets/WDscheduler.png';
+import Project5 from '../assets/comingsoon.jpg';
+import Project6 from '../assets/comingsoon.jpg';
+
 
 const Projects = () => {
   return (
@@ -12,16 +15,102 @@ const Projects = () => {
     <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>Projects</p>
     <p className='py-6'>Please Check out some of my recent projects</p>
 </div>
-<div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-    <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center'>
-        <div>
-          
-            <span>
+{/*Container*/}
+<div 
+className=' grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+    {/*Grid*/}
+    <div style={{backgroundImage: `url(${Project1}`}} 
+    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center content-div'>
+       
+       {/* HOVER EFFECTS*/}
+        <div className='opacity-8 group-hover:opacity-100'>
+                      <span className='text-2xl font-bold text-white tracking-wider'>
                 
             </span>
-            <div>
-                <a href='/'> <button></button></a>
-                <a href='/'> <button></button></a>
+            <div className='pt-8 text-center'>
+                <a href="/"> <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button></a>
+
+                <a href="/"> <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button></a>
+            </div>
+            </div>  
+    
+    </div>
+     <div style={{backgroundImage: `url(${Project2}`}} 
+    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center content-div'>
+       
+       {/* HOVER EFFECTS*/}
+        <div className='opacity-8 group-hover:opacity-100'>
+                      <span className='text-2xl font-bold text-white tracking-wider'>
+                
+            </span>
+            <div className='pt-8 text-center'>
+                <a href="/"> <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button></a>
+
+                <a href="/"> <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button></a>
+            </div>
+            </div>  
+    
+    </div>
+     <div style={{backgroundImage: `url(${Project3}`}} 
+    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center content-div'>
+       
+       {/* HOVER EFFECTS*/}
+        <div className='opacity-8 group-hover:opacity-100'>
+                      <span className='text-2xl font-bold text-white tracking-wider'>
+                
+            </span>
+            <div className='pt-8 text-center'>
+                <a href="/"> <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button></a>
+
+                <a href="/"> <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button></a>
+            </div>
+            </div>  
+    
+    </div>
+     <div style={{backgroundImage: `url(${Project4}`}} 
+    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center content-div'>
+       
+       {/* HOVER EFFECTS*/}
+        <div className='opacity-8 group-hover:opacity-100'>
+                      <span className='text-2xl font-bold text-white tracking-wider'>
+                
+            </span>
+            <div className='pt-8 text-center'>
+                <a href="/"> <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button></a>
+
+                <a href="/"> <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button></a>
+            </div>
+            </div>  
+    
+    </div>
+    <div style={{backgroundImage: `url(${Project5}`}} 
+    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center content-div'>
+       
+       {/* HOVER EFFECTS*/}
+        <div className='opacity-8 group-hover:opacity-100'>
+                      <span className='text-2xl font-bold text-white tracking-wider'>
+                
+            </span>
+            <div className='pt-8 text-center'>
+                <a href="/"> <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button></a>
+
+                <a href="/"> <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button></a>
+            </div>
+            </div>  
+    
+    </div>
+    <div style={{backgroundImage: `url(${Project6}`}} 
+    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center content-div'>
+       
+       {/* HOVER EFFECTS*/}
+        <div className='opacity-8 group-hover:opacity-100'>
+                      <span className='text-2xl font-bold text-white tracking-wider'>
+                
+            </span>
+            <div className='pt-8 text-center'>
+                <a href="/"> <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button></a>
+
+                <a href="/"> <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button></a>
             </div>
             </div>  
     
